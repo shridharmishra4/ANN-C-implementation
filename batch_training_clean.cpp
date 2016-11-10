fe8b742e130bbb22deb85bc4de3e0a79cdfca2d0
shridharmishra4 <shridharmishra4@gmail.com>
2016-11-10 15:54:59 +0530

    cleaner code

--- batch_training_clean.cpp
+++ batch_training_clean.cpp
@@ -8,17 +8,23 @@
 #define hidden2  10
 #define output 1
 #define numPatterns 1999
-#define numEpochs 1500
-#define stoping_error 0.01
+#define numEpochs 2000
+#define stoping_error 0.03
 #define lR 0.005
-#define n 10
+#define n 3
 
-double buffer[n]= {0};
+double buffer[n] = {0};
 
-int x = 0, y = 0, i = 0, j = 0,patNum = 0;
-double weights1[input][hidden1]= {{1,1},{1,1}};
-double weights2[hidden1][hidden2]= {{1,1},{1,1}};
-double weights3[hidden2][output]= {{1},{1}};
+int x = 0, y = 0, i = 0, j = 0, patNum = 0;
+double weights1[input][hidden1] = {{1, 1},
+    {1, 1}
+};
+double weights2[hidden1][hidden2] = {{1, 1},
+    {1, 1}
+};
+double weights3[hidden2][output] = {{1},
+    {1}
+};
 
 double bias1[hidden1] = {0};
 double bias2[hidden2] = {0};
@@ -35,10 +41,10 @@ double weightedsum = 0.0;
 double output_delta[output] = {0.0};
 double inputdelta[input] = {0.0};
 double errThisPat[output] = {0.0};
-double RMSerror = 0.0;
-double netoutputdelta[numPatterns][output]= {0.0};
-double nethidden2delta[numPatterns][hidden2]= {0.0};
-double nethidden1delta[numPatterns][hidden1]= {0.0};
+double RMSerrortrain = 0.0, RMSerrorvalidation = 0.0;
+double netoutputdelta[numPatterns][output] = {0.0};
+double nethidden2delta[numPatterns][hidden2] = {0.0};
+double nethidden1delta[numPatterns][hidden1] = {0.0};
 
 double dc_dw_hidden1[numPatterns][hidden1] = {0.0};
 double dc_dw_hidden2[numPatterns][hidden2] = {0.0};
@@ -48,29 +54,27 @@ double dc_dw_output[numPatterns][output] = {0.0};
 double trainInputs[numPatterns][input] = {0};
 double trainOutput[numPatterns][output] = {0};
 
-double validationinput[numPatterns][input]= {0};
-double validationoutput[numPatterns][output]= {0};
+double validationinput[numPatterns][input] = {0};
+double validationoutput[numPatterns][output] = {0};
 
 double testinput[numPatterns][input] = {0};
 double testoutput[numPatterns][output] = {0};
-double inputset1[numPatterns]= {0};
-double inputset2[numPatterns]= {0};
-double inputset3[numPatterns]= {0};
-double outputset[numPatterns]= {0};
+double inputset1[numPatterns] = {0};
+double inputset2[numPatterns] = {0};
+double inputset3[numPatterns] = {0};
+double outputset[numPatterns] = {0};
 
 
-FILE * pFile;
+FILE *pFile;
 
 //// functions ////
 void initWeights();
 
-void calcNet();
+void forwardPass();
 
 void calcOverallError();
 
 void displayResults();
-
-
 
 
 //***********************************
@@ -90,7 +94,7 @@ double sigmoid(double x)
 }
 
 ///Forward Pass.
-void calcNet(void)
+void forwardPass(double inputarray[][input], double outputarray[][output])
 {
     ///calculate the outputs of the hidden layer 1 neurons
     int i = 0, j = 0;
@@ -100,7 +104,7 @@ void calcNet(void)
 
         for (j = 0; j < input; ++j)
         {
-            hiddenVal1[i] = hiddenVal1[i] + (trainInputs[patNum][j] * weights1[j][i]);
+            hiddenVal1[i] = hiddenVal1[i] + (inputarray[patNum][j] * weights1[j][i]);
 
         }
         hiddenVal1[i] = sigmoid(hiddenVal1[i]);
@@ -142,72 +146,13 @@ void calcNet(void)
 
     for (i = 0; i < output; ++i)
     {
-        errThisPat[i] =  trainOutput[patNum][i] - outPred[i];
+        errThisPat[i] = outputarray[patNum][i] - outPred[i];
         //printf("e %lf\t",errThisPat[i]);
     }
 
 
 }
 
-void calcNetValidate(void)
-{
-    ///calculate the outputs of the hidden layer 1 neurons
-    int i = 0, j = 0;
-    errThisPat[0] = 0;
-    for (i = 0; i < hidden1; ++i)
-    {
-        hiddenVal1[i] = 0.0 + bias1[i];
-
-        for (j = 0; j < input; ++j)
-        {
-            hiddenVal1[i] = hiddenVal1[i] + (validationinput[patNum][j] * weights1[j][i]);
-
-        }
-        hiddenVal1[i] = sigmoid(hiddenVal1[i]);
-        //printf("Hidden 1 activation\t%lf\n",hiddenVal1[i]);
-    }
-    //output of hidden layer 2 neurons
-    for (i = 0; i < hidden2; ++i)
-    {
-        hiddenVal2[i] = 0.0 + bias2[i];
-
-        for (j = 0; j < hidden1; ++j)
-        {
-
-            hiddenVal2[i] = hiddenVal2[i] + (hiddenVal1[j] * weights2[j][i]);
-        }
-        hiddenVal2[i] = sigmoid(hiddenVal2[i]);
-        //printf("Hidden 2 activation\t%lf\n",hiddenVal2[i]);
-        //printf("%lf\t", hiddenVal2[i]);
-
-    }
-    //printf("\n");
-
-    //calculate the output of the network
-
-    for (i = 0; i < output; ++i)
-    {
-        outPred[i] = 0.0 + bias3[0];
-
-        for (j = 0; j < hidden2; ++j)
-        {
-            outPred[i] = outPred[i] + (hiddenVal2[j] * weights3[j][i]);
-        }
-        //printf("\n output activation %lf  ", outPred[i]);
-        outPred[i] = sigmoid(outPred[i]); /// Thats the individual output layer neuron prediction
-        //printf("\n output activation %lf\n", outPred[i]);
-
-
-    }
-
-    for (i = 0; i < output; ++i)
-    {
-        errThisPat[i] =  validationoutput[patNum][i] - outPred[i];
-        //printf("e %lf\t",errThisPat[i]);
-    }
-
-
-}
 
 
 void backprop()
@@ -219,7 +164,7 @@ void backprop()
     //printf("\nInside backprop at %d pattern \n\n",patNum);
     for (i = 0; i < output; ++i)
     {
-        output_delta[i] = -(trainOutput[patNum][i]-outPred[i]) * outPred[i] * (1 - outPred[i]);//output neuron delta
+        output_delta[i] = -(trainOutput[patNum][i] - outPred[i]) * outPred[i] * (1 - outPred[i]);//output neuron delta
         netoutputdelta[patNum][i] = output_delta[i];
         //sumdeltaoutput[i] = output_delta[i];
         //printf("netoutdelta[%d][%d] = %lf\n",patNum,i,output_delta[i]);
@@ -302,10 +247,9 @@ void sum_and_update()
     double sumdeltahidden1[hidden1] = {0.0};
 
 
-
-    for(i=0; i<output; ++i)
+    for (i = 0; i < output; ++i)
     {
-        for (j=0; j<patNum+1; ++j)
+        for (j = 0; j < patNum + 1; ++j)
         {
             sumdeltaoutput[i] += netoutputdelta[j][i];
             sum_dc_dw_output[i] += dc_dw_output[j][i];
@@ -314,9 +258,9 @@ void sum_and_update()
         //printf("\nsum_delta_output %lf\n", sum_dc_dw_output[i]);
     }
 
-    for(i=0; i<hidden2; ++i)
+    for (i = 0; i < hidden2; ++i)
     {
-        for (j=0; j<patNum+1; ++j)
+        for (j = 0; j < patNum + 1; ++j)
         {
             sumdeltahidden2[i] += nethidden2delta[j][i];
             sum_dc_dw_hidden2[i] += dc_dw_hidden2[j][i];
@@ -326,9 +270,9 @@ void sum_and_update()
         //printf("\nsum_delta_hidden2 %lf\n", sum_dc_dw_hidden2[i]);
     }
 
-    for(i=0; i<hidden1; ++i)
+    for (i = 0; i < hidden1; ++i)
     {
-        for (j=0; j<patNum+1; ++j)
+        for (j = 0; j < patNum + 1; ++j)
         {
             sumdeltahidden1[i] += nethidden1delta[j][i];
             sum_dc_dw_hidden1[i] += dc_dw_hidden1[j][i];
@@ -341,16 +285,16 @@ void sum_and_update()
     /// Bias update.
     for (i = 0; i < output; ++i)
     {
-        bias3[i] -= lR*sumdeltaoutput[i] ;
+        bias3[i] -= lR * sumdeltaoutput[i];
     }
     for (i = 0; i < hidden2; ++i)
     {
-        bias2[i] -= lR*sumdeltahidden2[i] ;
+        bias2[i] -= lR * sumdeltahidden2[i];
         //printf("\nbias 2 %lf\t",bias2[i]);
     }
     for (i = 0; i < hidden1; ++i)
     {
-        bias1[i] -= lR*sumdeltahidden1[i] ;
+        bias1[i] -= lR * sumdeltahidden1[i];
     }
 
     /// update weights
@@ -361,7 +305,7 @@ void sum_and_update()
     {
         for (j = 0; j < hidden1; ++j)
         {
-            weights1[i][j] -= lR*sum_dc_dw_hidden1[j];
+            weights1[i][j] -= lR * sum_dc_dw_hidden1[j];
             //printf("%lf \t",weights1[i][j]);
         }
         //printf("\n");
@@ -372,7 +316,7 @@ void sum_and_update()
     {
         for (j = 0; j < hidden2; ++j)
         {
-            weights2[i][j] -= lR*sum_dc_dw_hidden2[j];
+            weights2[i][j] -= lR * sum_dc_dw_hidden2[j];
             //printf("%lf \t",weights2[i][j]); //Calculated gradient * Old activation
         }
         //printf("\n");
@@ -382,7 +326,7 @@ void sum_and_update()
     {
         for (j = 0; j < output; ++j)
         {
-            weights3[i][j] -= lR*sum_dc_dw_output[j];
+            weights3[i][j] -= lR * sum_dc_dw_output[j];
             //printf("%lf \t",weights3[i][j]);
         }
         //printf("\n");
@@ -395,7 +339,7 @@ void sum_and_update()
 double randrange()
 {
 
-    double r = 4 * sqrt(6.0 / (input+output)); //for Sigmoid
+    double r = 4 * sqrt(6.0 / (input + output)); //for Sigmoid
     const double MIN_RAND = 0, MAX_RAND = 1;
     const double range = MAX_RAND - MIN_RAND;
     double random = range * ((((double) rand()) / (double) RAND_MAX)) + MIN_RAND;
@@ -438,85 +382,64 @@ void initWeights(void)
 // display results
 void displayResults(void)
 {
-    for (int i = 0; i < numPatterns; ++i)
+    for (int i = 0; i < numPatterns/7; ++i)
     {
         patNum = i;
-        calcNet();
+        forwardPass(testinput,testoutput);
         for (j = 0; j < output; ++j)
         {
-            printf("pat = %d actual = %lf neural model = %lf err = %lf\n", patNum + 1, trainOutput[patNum][j],outPred[j], errThisPat[j]);
+            printf("pat = %d actual = %lf neural model = %lf err = %lf\n", patNum + 1, testoutput[patNum][j],
+                   outPred[j], errThisPat[j]);
         }
     }
 }
 
-void displayResultsValidation(void)
-{
-    //double validationrms = 0.0;
-    FILE *validationout;
-    validationout = fopen("validate_out","w");
-    RMSerror = 0.0;
-    for (int i = 0; i <= numPatterns/5; ++i)
-    {
-
-        patNum = i*5;
-        calcNetValidate();
-        for (j = 0; j < output; ++j)
-        {
-            printf("pat = [%d] actual = %lf neural model = %lf err = %lf\n", (patNum)/5, validationoutput[patNum][j],outPred[j],errThisPat[j]);
-            RMSerror = (RMSerror + (errThisPat[j] * errThisPat[j])) / output;
-            fprintf(validationout,"%lf %lf\n",validationoutput[patNum][j],outPred[j]);
-            //validationrms =
-        }
-
-    }
-    RMSerror = RMSerror / numPatterns;
-    RMSerror = sqrt(RMSerror);
-    printf("\n\nRMS Error of validation data set = %lf\n",RMSerror);
-}
 
 //************************************
-// calculate the overall error
+// calculate the overall error for triaining set
 void calcOverallError(void)
 {
-    RMSerror = 0.0;
+    ///Calculate error during traing
+    RMSerrortrain = 0.0;
     for (int i = 0; i < numPatterns; ++i)
     {
         patNum = i;
-        calcNet();
+        forwardPass(trainInputs,trainOutput);
         for (j = 0; j < output; ++j)
         {
 
-            RMSerror = (RMSerror + (errThisPat[j] * errThisPat[j])) / output;
+            RMSerrortrain = (RMSerrortrain + (errThisPat[j] * errThisPat[j])) / output;
         }
     }
-    RMSerror = RMSerror / numPatterns;
-    RMSerror = sqrt(RMSerror);
+    RMSerrortrain = RMSerrortrain / numPatterns;
+    RMSerrortrain = sqrt(RMSerrortrain);
 }
 
 
 void calcValidationError(void)
 {
-    RMSerror = 0.0;
-    for (int i = 0; i < numPatterns/5; ++i)
+    ///Calculate error during validation
+    RMSerrorvalidation = 0.0;
+    for (int i = 0; i <= numPatterns / 5; ++i)
     {
-        patNum = i*5;
-        calcNetValidate();
+        patNum = i * 5;
+        forwardPass(validationinput,validationoutput);
         for (j = 0; j < output; ++j)
         {
 
-            RMSerror = (RMSerror + (errThisPat[j] * errThisPat[j])) / output;
+            RMSerrorvalidation = (RMSerrorvalidation + (errThisPat[j] * errThisPat[j])) / output;
         }
     }
-    RMSerror = RMSerror / numPatterns;
-    RMSerror = sqrt(RMSerror);
+    RMSerrorvalidation = RMSerrorvalidation / (numPatterns/5);
+    RMSerrorvalidation = sqrt(RMSerrorvalidation);
 
 }
 
-double Filter( double sample)
+double Filter(double sample)
 {
 
-    static  char oldest = 0;
-    static  double sum=0;
+    static char oldest = 0;
+    static double sum = 0;
 
     sum -= buffer[oldest];
     sum += sample;
@@ -524,35 +447,28 @@ double Filter( double sample)
     oldest += 1;
     if (oldest >= n) oldest = 0;
 
-    return sum/n;
+    return sum / n;
 }
-
-
 
 
 void initData()
 {
-    FILE *fin,*fout,*asdf,*ftest,*fvalidate;
-    double inp[7],op;
-    int count=0,i,j;
-    double *minmax1,*minmax2,*minmax3,*minmaxout;
+    FILE *fin, *fout, *asdf, *ftest, *fvalidate;
+    double inp[7], op;
+    int count = 0, i, j;
+    double *minmax1, *minmax2, *minmax3, *minmaxout;
 
     fin = fopen("training_input", "r");
     fout = fopen("training_output", "r");
-    //asdf = fopen("input","w");
-    //fvalidate = fopen("validate","w");
-    //ftest = fopen("test","w");
     printf("Init inputs");
-    //pFile = fopen ("f1.txt","w");
-    while(!feof(fin))
+    while (!feof(fin))
     {
 
-        fscanf(fin,"%lf %lf %lf %lf %lf %lf %lf\n",&inp[0],&inp[1],&inp[2],&inp[3],&inp[4],&inp[5],&inp[6]);
-        fscanf(fout,"%lf\n",&op);
+        fscanf(fin, "%lf %lf %lf %lf %lf %lf %lf\n", &inp[0], &inp[1], &inp[2], &inp[3], &inp[4], &inp[5], &inp[6]);
+        fscanf(fout, "%lf\n", &op);
 
-        if (count%5 == 0)
+        if (count % 5 == 0)
         {
-            //printf("hel %d\n",count);
             validationinput[count][0] = inp[0];
             validationinput[count][1] = inp[1];
             validationinput[count][2] = inp[2];
@@ -561,11 +477,8 @@ void initData()
             validationinput[count][5] = inp[5];
             validationinput[count][6] = inp[6];
             validationoutput[count][0] = op;
-            //printf("%d %lf %lf \n",count,inp[0],op);
-            //fprintf(fvalidate,"%lf %lf %lf %lf %lf %lf %lf\n",inp[0],inp[1],inp[2],inp[3],inp[4],inp[5],inp[6]);
         }
-
-        else if(count%7 == 0)
+        else if (count % 7 == 0)
         {
             testinput[count][0] = inp[0];
             testinput[count][1] = inp[1];
@@ -575,10 +488,8 @@ void initData()
             testinput[count][5] = inp[5];
             testinput[count][6] = inp[6];
             testoutput[count][0] = op;
-            //fprintf(ftest,"%lf %lf %lf %lf %lf %lf %lf\n",inp[0],inp[1],inp[2],inp[3],inp[4],inp[5],inp[6]);
 
         }
-
         else
         {
             trainInputs[count][0] = inp[0];
@@ -589,14 +500,13 @@ void initData()
             trainInputs[count][5] = inp[5];
             trainInputs[count][6] = inp[6];
             trainOutput[count][0] = op;
-            //fprintf(asdf,"%lf %lf %lf %lf %lf %lf %lf\n",inp[0],inp[1],inp[2],inp[3],inp[4],inp[5],inp[6]);
 
         }
 
         count++;
-        if(count>numPatterns)
+        if (count > numPatterns)
         {
-            printf("%d\n",count);
+            printf("%d\n", count);
             break;
         }
 
@@ -604,23 +514,18 @@ void initData()
 
     fclose(fin);
     fclose(fout);
-    //fclose(asdf);
-    //fclose(fvalidate);
-    //fclose(ftest);
+
 
 }
-
-
 
 
 int main(void)
 {
     double moving_avg = 0.0;
     // seed random number function
-
-    //pFile = fopen ("f.txt","w");
-
-
+    FILE *validationerrorfile;
+    //trainerror = fopen ("err_train.txt","w");
+    validationerrorfile = fopen("err_validate.txt","w");
 
     srand(time(NULL));
     // initiate the weights
@@ -641,56 +546,47 @@ int main(void)
             patNum = i;
             //calculate the current network output
             //and error for this pattern
-            calcNet();
-            //printf("\nAfter % epoch, error = %lf \n",i,errThisPat);
+            forwardPass(trainInputs,trainOutput);
             backprop();
-            //printf("net op delta at %d = %lf\n",patNum,netoutputdelta[patNum][0]);
-
-
-//            printf("------------------------------------------------------------\n");
-
 
         }
-//        printf("------------------------------------------------------------\n");
-//        printf("\nsum error\n");
-//        printf("------------------------------------------------------------\n");
         sum_and_update();
-        //printf("sum output delta %lf ",sumdeltaoutput[0]);
-        //display the overall network error after each epoch
         ///to do: Add save network.
         calcOverallError();
-        printf("epoch %d error = %lf \n",j,RMSerror);
+        calcValidationError();
+
+        //Write to file for graph plotting
+        fprintf(validationerrorfile,"%lf %lf \n",RMSerrortrain,RMSerrorvalidation);
+        printf("After %d epoch, RmsTrain = %lf RMSvalidate = %lf \n",j,RMSerrortrain,RMSerrorvalidation);
 
 
-        //calcValidationError();
-        //moving_avg = Filter(RMSerror);
-        //float truncated = trunc(moving_avg*10000)/10000;
-        //printf("epoch %d error = %lf \n",j,RMSerror);
-
-//        if(RMSerror > moving_avg && j>1000 )
-//        {
-//            if (RMSerror>moving_avg+0.3){
-//                continue;
-//            }
-//            else{
-//                printf("Moving avg = %lf\n",moving_avg);
-//                printf("%lf\n",RMSerror);
-//                displayResults();
-//                exit(0);
-//            }
-//        }
-
-        //fprintf(pFile,"%f\n", RMSerror);
+        ///Early Stopping
+        moving_avg = Filter(RMSerrorvalidation);
+        if(RMSerrorvalidation<stoping_error)
+        {
+            if((moving_avg-RMSerrorvalidation)<0.00001 )
+            {
+                //            if (RMSerrorvalidation > moving_avg+0.1){
+                //                continue;
+                //            }
+                //            else{
+                printf("difference => %f - %f = %lf\n",moving_avg,RMSerrorvalidation,(moving_avg-RMSerrorvalidation));
+                //printf("Moving avg = %lf\n",moving_avg);
+                //printf("%lf\n",RMSerrorvalidation);
+                //displayResults();
+                exit(0);
+                //}
+            }
+        }
 
     }
-
-    //training has finished
-    //display the results
+    fclose(validationerrorfile);
+    ///training has finished
+    ///display the results from training set
     printf("\n------------------------------------------------------------\n");
     printf("------------------------------------------------------------\n");
     printf("------------------------------------------------------------\n");
 
-    displayResultsValidation();
     //displayResults();
     //fclose(pFile);
 
@@ -700,30 +596,3 @@ int main(void)
 }
 
 
-
-//double * minmax(double v[])
-//{
-//
-//    double min1=9999999,max1 = -9999,len=0;
-//    static double out[2] = {0,0};
-//    int i=0;
-//    for(int i=0; v[i]; i++)
-//        len++;
-//
-//    for(i=0; i<len; i++)
-//    {
-//
-//        if(v[i]>max1)
-//        {
-//            max1=v[i];
-//        }
-//        if(v[i]<min1)
-//        {
-//            min1=v[i];
-//        }
-//    }
-//    out[0] = min1;
-//    out[1] = max1;
-//    printf("inside func%lf %lf\n",out[0],out[1]);
-//    return out;
-//}
